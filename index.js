115764cc58b7f02410d26084d3b0fb7bd28b2683
Ogpavan <pawantwp@gmail.com>
2024-08-20 20:40:53 +0530

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+// server.js
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API running');
+})
+// Use routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/students', require('./routes/student'));
+app.use('/api/attendance', require('./routes/attendance'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
